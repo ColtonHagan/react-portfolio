b0de8b1f6068c81419524a50019907dc5615cff4
--list <coltonmhagan@gmail.com>
2023-05-31 20:01:36 -0700

    setup gh-pages

--- src/views/Contact/components/ContactItem/index.js
+++ src/views/Contact/components/ContactItem/index.js
@@ -11,17 +11,17 @@ import "./index.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ContactItem = ({ logo, title, text }) => {
-    return (
-        <div className='contact-item'>
-            <div className='title-section'>
-                <FontAwesomeIcon className="logo" icon={logo} />
-                {title}
-            </div>
-            <div className='text-section'>
-                {text}
-            </div>
-        </div>
-    )
+  return (
+    <div className='contact-item'>
+      <div className='title-section'>
+        <FontAwesomeIcon className="logo" icon={logo} />
+        {title}
+      </div>
+      <div className='text-section'>
+        {text}
+      </div>
+    </div>
+  )
 }
 
 export default ContactItem;
